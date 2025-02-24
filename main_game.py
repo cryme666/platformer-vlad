11ab3e6a72c31ecb08b6e864bfefaaf0602ceb29
cryme666 <125838508+cryme666@users.noreply.github.com>
2025-02-24 20:02:29 +0200

    upd

--- main_game.py
+++ main_game.py
@@ -142,6 +142,7 @@ class Player(pygame.sprite.Sprite):
 
     def make_hit(self):
         self.hit = True
+        
 
     def hit_head(self):
         self.y_vel *= -1 
@@ -165,6 +166,40 @@ class Block(Object):
         block = get_block(size)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
+
+class Fire(Object):
+    def __init__(self,x,y,width,height):
+        self.ANIMATION_DELAY = 3
+        super().__init__(x,y,width,height,'fire')
+        self.fire = load_sprite_sheets('Traps','Fire',width,height)
+        self.image = self.fire['on'][0]
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.animation_count = 0
+        self.animation_name = 'on'
+
+    def on(self):
+        self.animation_name = 'on'
+
+    def off(self):
+        self.animation_name = 'off'
+
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        print(sprite_index)
+
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.rect = self.image.get_rect(topleft=(self.rect.x,self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+
+
+
 
 
 
@@ -253,7 +288,22 @@ def main(window):
 
     floor = [Block(i * block_size, HEIGHT - block_size, block_size)
              for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
-    objects = [*floor,Block(0,HEIGHT-block_size*2,block_size),Block(block_size*3,HEIGHT-block_size*4,block_size)]
+    
+    fire = Fire(block_size*5 - 20,HEIGHT-block_size-64,16,32)
+    fire.on()
+
+    objects = [
+        *floor,
+        Block(0,HEIGHT-block_size*2,block_size),
+        Block(block_size*5,HEIGHT-block_size*4,block_size),
+        Block(block_size*3,HEIGHT-block_size*2,block_size),
+        fire
+        ]
+
+    n_block = 5
+    for i in range(2,n_block*2-1):
+        objects.append(Block(0,HEIGHT-block_size*i,block_size))
+    
     offset_x = 0
     scroll_area_width = 200
 
@@ -271,6 +321,7 @@ def main(window):
                     player.jump()
 
         player.loop(FPS)
+        fire.loop()
         handle_move(player, objects)
         draw(window, background, bg_image, player, objects, offset_x)
 
